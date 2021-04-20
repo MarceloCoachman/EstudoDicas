7ccd06977839438bf4160e15c9d8ac0a4be3295a
MarceloCoachman <marcelo.coachman@outlook.com>
2021-04-20 19:43:41 -0300

    Desafio _ DIO
    A Digital Innovation One  propõe o desafio de programação .net;

---
+++ Desafios/DIO - Digitalinnovation.One/C Sharp/Introdução a Programação com .NET/Programa para Validação de Notas/Programa_para_Validação_de_Notas_Resolvido.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programa_para_Validação_de_Notas
+{
+    class Programa_para_Validação_de_Notas_Resolvido
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
