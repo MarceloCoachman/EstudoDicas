4e60daeef573ca878aec988d43e3e67d6bc83f56
MarceloCoachman <marcelo.coachman@outlook.com>
2021-05-29 19:08:45 -0300

    update
    Continuando o curso

--- Angular/course-manager/src/app/courses/couse-list.component.ts
+++ Angular/course-manager/src/app/courses/couse-list.component.ts
@@ -1,10 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import {Couse} from './course';
 
 @Component({
     selector: 'app-course-list',
-    template: '<h2> Couser List </h2>'
+    templateURl: './course-list.component.html'
 })
 
-export class CourseListComponent {
+export class CourseListComponent implements OnInit{
+    courses:Course[] = [];
+    ngOnInit(): void{
+        this.courses= [
+            {
+                id:1,
+                name: 'Angular: forms',
+                imageUrl:'',
+                price: 99.90,
+                code: 'zz2-324',
+                duration:120,
+                rating: 5.5
+            },
+            {
+                id:2,
+                name: 'Angular: HTML',
+                imageUrl:'',
+                price: 60.00,
+                code: 'XX4-4375',
+                duration:60,
+                rating: 5.0
+            }
+        ]
+    }
 
 }
