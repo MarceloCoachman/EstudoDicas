1f4cbd81f5b7ecfdb67f6a35aef2373cd794d392
MarceloCoachman <marcelo.coachman@outlook.com>
2021-09-13 18:26:30 -0300

    Update Program.cs

--- Desafios/DIO - Digitalinnovation.One/C Sharp/Consumo Médio do Automóvel/Média do Automóvel/Média do Automóvel/Program.cs
+++ Desafios/DIO - Digitalinnovation.One/C Sharp/Consumo Médio do Automóvel/Média do Automóvel/Média do Automóvel/Program.cs
@@ -11,7 +11,7 @@ namespace Média_do_Automóvel
             distancia = Convert.ToInt32(Console.ReadLine());
             combustivelGasto = Convert.ToDouble(Console.ReadLine());
 
-            consumoMedio =  distancia/combustivelGasto; //atribua a formula para que o codigo funcione corretamente
+            consumoMedio =  distancia/(.1*combustivelGasto); //atribua a formula para que o codigo funcione corretamente
 
             Console.WriteLine("{0:0.000} km/l", consumoMedio);
 
