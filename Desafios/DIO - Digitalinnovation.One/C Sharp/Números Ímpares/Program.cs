16c95be24f051eed0d89ae5b521566a8d7785c6b
MarceloCoachman <marcelo.coachman@outlook.com>
2021-09-14 16:39:20 -0300

    Desafio Números Ímpares
    Commit Final

---
+++ Desafios/DIO - Digitalinnovation.One/C Sharp/Números Ímpares/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Números_Ímpares
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            //complete seu codigo
+            for ( int j = 1;j <= n; j++ ) //complete seu codigo
+            {
+                if (j%2 != 0)
+                Console.WriteLine(j);
+            }
+
+            
+        }
+
+    }
+}
