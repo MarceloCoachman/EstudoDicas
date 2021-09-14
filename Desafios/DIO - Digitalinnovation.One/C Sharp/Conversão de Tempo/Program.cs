d923180d67b99b78994ec24337adc9bb38f7222e
MarceloCoachman <marcelo.coachman@outlook.com>
2021-09-14 17:09:52 -0300

    Desafio Conversão de Tempo
    Commit Final

---
+++ Desafios/DIO - Digitalinnovation.One/C Sharp/Conversão de Tempo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Conversão_de_Tempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timeInSeconds = int.Parse(Console.ReadLine());
+
+            //continue a solucao
+            var hours = timeInSeconds / 3600;
+            //timeInSeconds =  hours;
+            var minutes = (timeInSeconds % 3600)/60 ;
+            var seconds = (timeInSeconds % 3600)%60 ;
+
+            Console.WriteLine($"{hours}:{minutes}:{seconds}");
+        }
+    }
+}
