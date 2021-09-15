2559011f083c54720d752452a7f11611c1b7fb41
MarceloCoachman <marcelo.coachman@outlook.com>
2021-09-15 03:14:01 -0300

    Desafio O Maior
    Commit Final

---
+++ Desafios/DIO - Digitalinnovation.One/C Sharp/O Maior/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace O_Maior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+            //complete declarando as variáveis
+            int a = Int32.Parse(vet[0]);
+            int b = Int32.Parse(vet[1]);
+            int c = Int32.Parse(vet[2]);
+
+            int MAIORAB = (a + b +Math.Abs(a - b)) / 2;
+
+            int MAIOR = (MAIORAB + c + Math.Abs(MAIORAB - c)) / 2;
+
+            Console.WriteLine($"{MAIOR} eh o maior");
+
+        }
+    }
+}
