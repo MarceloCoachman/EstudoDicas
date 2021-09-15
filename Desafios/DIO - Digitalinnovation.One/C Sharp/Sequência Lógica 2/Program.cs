999c1d2ad8b5308b999347d041fb67d808501ce8
MarceloCoachman <marcelo.coachman@outlook.com>
2021-09-15 05:12:43 -0300

    Desafio Sequência Lógica 2
    Commit Final

---
+++ Desafios/DIO - Digitalinnovation.One/C Sharp/Sequência Lógica 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Sequência_Lógica_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] vet = Console.ReadLine().Split(" ") ;
+            int x = Int32.Parse(vet[0]); // itens por linha 
+            int y = Int32.Parse(vet[1]); // numero total
+            
+            //complete o código com sua solução
+
+            for ( int i = 1; i <=  y; i++ )
+            {
+                Console.Write(i);
+                if (i % x == 0             )
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
+
