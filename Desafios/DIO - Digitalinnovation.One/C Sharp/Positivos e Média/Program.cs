57a4b1576c6563d7e2a14e0e8bce01ad3c476656
MarceloCoachman <marcelo.coachman@outlook.com>
2021-09-21 23:00:47 -0300

    Positivos e Média
    Sem aprovação pela DIO. Possível erro no site.

---
+++ Desafios/DIO - Digitalinnovation.One/C Sharp/Positivos e Média/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Positivos_e_Média
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b = 0, avg;
+            int postive = 0;
+
+            for (int i = 0; i < 6; i++ )
+            {
+                a = double.Parse(Console.ReadLine());
+
+                if ( a > 0 )
+                {
+                    postive += 1;
+                    b += a;
+                }
+            }
+            avg = b / postive;
+            Console.WriteLine(avg);
+            Console.WriteLine("{0} valores positivos", postive);
+            //Console.WriteLine("{0:f1}",avg);
+            //Console.WriteLine("{0}", Math.Round(avg, 1);
+            //Console.WriteLine("{0}", Math.Round(avg, 1, MidpointRounding.AwayFromZero).ToString("0.0"));
+            //Console.WriteLine("{0}", Math.Round(avg, 1, MidpointRounding.ToEven).ToString("0.0"));
+            Console.ReadLine();
+        }
+
+    }
+}
