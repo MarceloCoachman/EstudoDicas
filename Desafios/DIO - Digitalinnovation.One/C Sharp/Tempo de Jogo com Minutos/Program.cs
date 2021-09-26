4bb90a66a56159fe77dca413153eeeef984d73f7
MarceloCoachman <marcelo.coachman@outlook.com>
2021-09-25 22:31:49 -0300

    Desafio Tempo de Jogo com Minutos
    Desafio para resolver .

---
+++ Desafios/DIO - Digitalinnovation.One/C Sharp/Tempo de Jogo com Minutos/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Tempo_de_Jogo_com_Minutos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+
+class {
+
+        public static void Main()
+        {
+            int a, b, c, d, x, y;
+            string[] s = Console.ReadLine().Split(' ');
+            a = int.Parse(s[0]);
+            x = int.Parse(s[1]);
+            b = int.Parse(s[2]);
+            y = int.Parse(s[3]);
+
+            if (             )
+            {
+
+                Console.WriteLine("O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)", d, c);
+            }
+            else if (         )
+            {
+
+                Console.WriteLine("O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)", d, c);
+            }
+            else if (         )
+            {
+
+                Console.WriteLine("O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)", d, c);
+            }
+            else if (          )
+            {
+
+                Console.WriteLine("O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)", d, c);
+            }
+            else if (         )
+            {
+
+                Console.WriteLine("O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)", d, c);
+            }
+            else if (            )
+            {
+
+                Console.WriteLine("O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)", d, c);
+            }
+            else if (          )
+            {
+
+                Console.WriteLine("O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)", d, c);
+            }
+            else if (              )
+            {
+
+                Console.WriteLine("O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)", d, c);
+            }
+            else if (                 )
+            {
+
+                Console.WriteLine("O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)", d, c);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
+
+/*
+ Intermediário
+ Princípios Básicos
+Desafio
+Leia a hora inicial, minuto inicial, hora final e minuto final de um jogo. A seguir calcule a duração do jogo.
+
+Obs: O jogo tem duração mínima de um (1) minuto e duração máxima de 24 horas.
+
+Entrada
+Quatro números inteiros representando a hora de início e fim do jogo.
+
+Saída
+Mostre a seguinte mensagem: “O JOGO DUROU XXX HORA(S) E YYY MINUTO(S)” .
+
+ 
+Exemplo de Entrada	Exemplo de Saída
+7 8 9 10            O JOGO DUROU 2 HORA(S) E 2 MINUTO(S)
+
+7 7 7 7             O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)
+
+7 10 8 9            O JOGO DUROU 0 HORA(S) E 59 MINUTO(S)
+
+*/
